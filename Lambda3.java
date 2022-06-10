a4525248fca5164f78538ccd91202173350d2708
Budhi <gurung.b1t@gmail.com>
2022-06-10 14:37:24 -0400

    predicate, function, consumer, and supplier are predefined functions in Lambda expression

--- Lambda3.java
+++ Lambda3.java
@@ -1,4 +1,9 @@
+import java.util.Date;
 import java.util.ArrayList;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class Lambda3 {
     public static void main(String[] args) {
@@ -14,6 +19,34 @@ public class Lambda3 {
         empList.add(new Employee(9, "Robert", 23, 70000.00));
         empList.add(new Employee(10, "Donald", 27, 85000.00));
     
+        Function<Double, Double> func = salary->salary + (0.1 * salary);
+        Predicate<Integer> pr = a->(a>30);
+        Predicate<Integer> pr1 = a->(a<=30);
+        Consumer<String> con = sc->System.out.println("Congratulation " + sc + "! on your salary bonus!");
+        Supplier<Date> sup = ()->new Date();
+
+        for (Employee employee : empList) {
+            if(pr.test(employee.getAge())){
+                System.out.println("Name : " + employee.getName() + " | New salary : " + func.apply(employee.getSalary()));
+                con.accept(employee.getName());
+                System.out.println(sup.get());
+            }
+        }
+
+        System.out.println("------------------------");
+        // for (Employee employee : empList) {
+        //     if(!(pr.test(employee.getAge()))){
+        //         System.out.println("Name : " + employee.getName() + " | salary : " + employee.getSalary());
+        //     }
+        // }
+
+        for (Employee employee : empList) {
+            if(pr1.test(employee.getAge())){
+                System.out.println("Name : " + employee.getName() + " | salary : " + employee.getSalary());
+            }
+        }
+
+
         
     }
 }
